f7899374f99b5e6ece41b6d52c1c6e2c077fcace
kholid101 <kholidfansuri@gmail.com>
2021-04-14 14:44:03 +0700

    v7

--- darkV3.js
+++ darkV3.js
@@ -1,26 +1,6 @@
-// dark dan light
-const mode = document.querySelector('.mode');
-let body = {
-    'helo': document.querySelector('body'),
-    'dark': false
+
+var change = document.getElementById("change");
+
+change.onclick = function(){
+    document.body.classList.toggle("dark-theme");
 }
-
-mode.addEventListener('click', ()=>{
-    if(body.dark === false){
-        body.dark = true;
-        body.helo.className = 'dark';
-        mode.textContent = 'light';
-    }
-    else{
-        body.dark = false;
-        body.helo.className = 'light';
-        mode.textContent = 'dark';
-    }
-});
-
-const btns = document.querySelectorAll('li > button');
-btns.forEach(btn =>{
-    btn.addEventListener('click', ()=>{
-        btn.classList.toggle('active');
-    });
-});
